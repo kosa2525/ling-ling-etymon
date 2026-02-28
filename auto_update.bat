fe1fcfb27e875fd29e676d1f9635c86f1964297b
kosa2525 <kouta.saito.0807@gmail.com>
2026-02-28 13:30:11 +0900

    Add maskable icon and separate icon purposes

---
+++ auto_update.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d "c:\Users\integ\OneDrive\デスクトップ\ling-ling-etymon"
+
+echo --- [1/3] 単語を生成中... ---
+python scripts\generate_etymology.py 1
+
+echo --- [2/3] GitHubへアップロード準備中... ---
+git add data.js essays.js
+
+echo --- [3/3] 公開サイトを更新中... ---
+git commit -m "Auto-update: New words/essays added via task scheduler"
+git push origin main
+
+echo --- 完了しました！ ---
